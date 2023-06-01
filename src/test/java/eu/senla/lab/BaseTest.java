bf822de5ee74e45dfa3419fb76f84afc147f315a
Dzianis Shapel <dzianis283@gmail.com>
2023-06-01 17:21:15 +0300

    do not work

--- src/test/java/eu/senla/lab/BaseTest.java
+++ src/test/java/eu/senla/lab/BaseTest.java
@@ -1,5 +1,6 @@
 package eu.senla.lab;
 
+import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import com.github.javafaker.Faker;
 import eu.senla.lab.api.ApiHelper;
@@ -17,6 +18,9 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+import static com.codeborne.selenide.Browsers.CHROME;
+import static com.codeborne.selenide.Browsers.FIREFOX;
+
 public class BaseTest {
 
     @Parameters({"browser"})
@@ -24,46 +28,10 @@ public class BaseTest {
     public void setUp(@Optional String browser) throws MalformedURLException {
         switch (browser) {
             case "chrome":
-                ChromeOptions options = new ChromeOptions();
-                options.setCapability("browserVersion", "113.0");
-                options.setCapability("selenoid:options", new HashMap<String, Object>() {{
-                    /* How to add test badge*/
-                    put("name", "Test badge...");
-                    /* How to set session timeout */
-                    put("sessionTimeout", "15m");
-                    /* How to set timezone */
-                    put("env", new ArrayList<String>() {{
-                        add("TZ=UTC");
-                    }});
-                    /* How to add "trash" button */
-                    put("labels", new HashMap<String, Object>() {{
-                        put("manual", "true");
-                    }});
-                    /* How to enable video recording */
-                    put("enableVideo", true);
-                }});
-                RemoteWebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), options);
+                Configuration.browser = CHROME;
                 break;
             case "firefox":
-                FirefoxOptions firefoxOptions = new FirefoxOptions();
-                firefoxOptions.setCapability("browserVersion", "113.0");
-                firefoxOptions.setCapability("selenoid:options", new HashMap<String, Object>() {{
-                    /* How to add test badge */
-                    put("name", "Test badge...");
-                    /* How to set session timeout */
-                    put("sessionTimeout", "15m");
-                    /* How to set timezone */
-                    put("env", new ArrayList<String>() {{
-                        add("TZ=UTC");
-                    }});
-                    /* How to add "trash" button */
-                    put("labels", new HashMap<String, Object>() {{
-                        put("manual", "true");
-                    }});
-                    /* How to enable video recording */
-                    put("enableVideo", true);
-                }});
-                RemoteWebDriver fdriver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), firefoxOptions);
+                Configuration.browser = FIREFOX;
                 break;
         }
     }
