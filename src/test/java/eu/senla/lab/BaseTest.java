b12424fcd7ebf9c8e85e79340f774b96b0e5ef71
dzianis <dzianis283@gmail.com>
2023-06-13 07:34:00 +0300

    vnc works

--- src/test/java/eu/senla/lab/BaseTest.java
+++ src/test/java/eu/senla/lab/BaseTest.java
@@ -1,11 +1,13 @@
 package eu.senla.lab;
 
+import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.WebDriverRunner;
 import com.github.javafaker.Faker;
 import eu.senla.lab.api.ApiHelper;
 import eu.senla.lab.objects.Employee;
-import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -14,22 +16,43 @@ import org.testng.annotations.Parameters;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.HashMap;
+
+import static com.codeborne.selenide.Browsers.CHROME;
+import static com.codeborne.selenide.Browsers.FIREFOX;
 
 public class BaseTest {
+
+    private RemoteWebDriver driver;
 
     @Parameters({"browser"})
     @BeforeMethod
     public void setUp(@Optional String browser) throws MalformedURLException {
         switch (browser) {
             case "chrome":
+                Configuration.browser = CHROME;
+        //        Configuration.browserSize = "1280x720";
+                ChromeOptions options = new ChromeOptions();
+                options.setCapability("selenoid:options", new HashMap<String, Object>() {{
+      //              put("enableVideo", true);
+                    put("enableVNC", true);
+                }});
+                driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), options);
+                break;
             case "firefox":
-                final DesiredCapabilities caps = new DesiredCapabilities();
-                caps.setBrowserName(browser);
-                caps.setCapability("enableVNC", true);
-                RemoteWebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), caps);
-                WebDriverRunner.setWebDriver(driver);
+                Configuration.browser = FIREFOX;
+        //        Configuration.browserSize = "1280x720";
+                FirefoxOptions firefoxOptions = new FirefoxOptions();
+                firefoxOptions.setCapability("browserVersion", "112.0");
+                firefoxOptions.setCapability("selenoid:options", new HashMap<String, Object>() {{
+      //              put("enableVideo", true);
+                    put("enableVNC", true);
+                }});
+                driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), firefoxOptions);
                 break;
         }
+        WebDriverRunner.setWebDriver(driver);
+        WebDriverRunner.getWebDriver().manage().window().maximize();
     }
 
     public Employee createEmployee() {
